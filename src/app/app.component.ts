e6fa6b4decb2c4954fbfa88b71c89b728f8c180d
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-09-01 23:04:01 -0500

    feat: added scroll event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {AfterViewInit, Component} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {MainComponent} from "./main/main.component";
 import {HowItWorksComponent} from "./how-it-works/how-it-works.component";
@@ -9,6 +9,7 @@ import {FooterComponent} from "./footer/footer.component";
 import {JoinComponent} from "./join/join.component";
 import {WhocanuseComponent} from "./whocanuse/whocanuse.component";
 import {AppRoutingModule} from "./app-routing.module";
+import {ViewportScroller} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,25 @@ import {AppRoutingModule} from "./app-routing.module";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'untitled';
+export class AppComponent implements AfterViewInit {
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        const fragment = this.router.parseUrl(this.router.url).fragment;
+        if (fragment) {
+          this.smoothScrollTo(fragment);
+        }
+      }
+    });
+  }
+
+  smoothScrollTo(fragment: string) {
+    const element = document.getElementById(fragment);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
